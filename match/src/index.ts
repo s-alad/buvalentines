be294b2e807d61cc826bfbee49a267b2d82742d4
s-alad <n7.saad@gmail.com>
2024-02-06 18:14:05 -0500

    add function to match

--- match/src/index.ts
+++ match/src/index.ts
@@ -1,6 +1,6 @@
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require('./buvservicekey.json');
+const serviceAccount = require('./config/buvservicekey.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
@@ -18,22 +18,42 @@ collectionRef.get()
         console.error('Error getting documents:', error);
     });
 
+interface Matchee {
+    name: string;
+    email: string;
+    age: number
+    gender: string;
+    message: string;
+}
 
 async function match() {
     const matchexample = {
         p1: {
             name: "Bob",
             email: "bob@bu.edu",
-            message: ""
+            age: 20,
+            gender: "male",
+            message: "hey"
         },
         p2: {
             name: "Alice",
-            email: "Alice@bu.edu",
+            email: "alice@bu.edu",
+            age: 20,
+            gender: "female",
             message: "hi"
         },
         shared: {
             interests: ["x1", "x2", "x3"],
-            ideal: ["y1", "y2", "y3"]
+            idealdate: ["y1", "y2", "y3"]
         }
+    }
+    const matchidexample = matchexample.p1.email + "+" + matchexample.p2.email;
+
+    // write this data to a firestore collection called "matched"
+    try {
+        await firestore.collection("matched").doc(matchidexample).set(matchexample);
+        console.log(`Match ${matchidexample} added to Firestore.`);
+    } catch (error) {
+        console.error("Error adding user to Firestore:", error);
     }
 }
