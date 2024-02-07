d565108425caecea0e016747e2b8187d0678ea7f
mochiakku <mochiakku@gmail.com>
2024-02-07 15:02:23 +0800

    fixed text

--- client/types/schema.ts
+++ client/types/schema.ts
@@ -7,7 +7,7 @@ export const matchschema: ZodType<formdata> = z
         name: z.string().min(2, "Name must be at least 2 characters").max(50, "Name must be between 2 and 50 characters"),
         age: z.number(
             { invalid_type_error: 'Enter a number' }
-        ).int().min(18, "You must be at least 18 years old").max(25, "Too old get a job!!"),
+        ).int().min(18, "You must be at least 18 years old").max(25, "Very old get a job!!"),
         gender: z.enum(Genders, {
             errorMap: (issue, ctx) => {
                 return {message: 'Please select your gender'};
